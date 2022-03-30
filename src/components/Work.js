9ddb56c4baa5c562c9bdf56c12c8aa46b20baa65
Roshan Dhaker <81019981+roshan65397@users.noreply.github.com>
2022-03-30 11:07:59 +0530

    Update Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -81,7 +81,7 @@ const Work = () => {
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/roshan65397/portfolio">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
